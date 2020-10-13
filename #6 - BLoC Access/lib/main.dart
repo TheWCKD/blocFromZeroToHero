37d82751c0b8037cfbf9370236e2cbf34305f5a6
Tiberiu Potec <potec.tiberiu@gmail.com>
2020-10-13 18:50:34 +0300

    finished bloc access #6

--- #6 - BLoC Access/lib/main.dart
+++ #6 - BLoC Access/lib/main.dart
@@ -2,37 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_concepts/logic/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_concepts/presentation/router/app_router.dart';
-import 'package:flutter_bloc_concepts/presentation/screens/home_screen.dart';
-import 'package:flutter_bloc_concepts/presentation/screens/second_screen.dart';
-import 'package:flutter_bloc_concepts/presentation/screens/third_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   final AppRouter _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        onGenerateRoute: _appRouter.onGenerateRoute,
       ),
-      onGenerateRoute: _appRouter.onGenerateRoute,
     );
-  }
-
-  @override
-  void dispose() {
-    _appRouter.dispose();
-    super.dispose();
   }
 }
--- #6 - BLoC Access/lib/presentation/router/app_router.dart
+++ #6 - BLoC Access/lib/presentation/router/app_router.dart
@@ -6,46 +6,31 @@ import 'package:flutter_bloc_concepts/presentation/screens/second_screen.dart';
 import 'package:flutter_bloc_concepts/presentation/screens/third_screen.dart';
 
 class AppRouter {
-  final _counterCubit = CounterCubit();
-
   Route onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child: HomeScreen(
-              title: "Home Screen",
-              color: Colors.blueAccent,
-            ),
+          builder: (_) => HomeScreen(
+            title: "Home Screen",
+            color: Colors.blueAccent,
           ),
         );
       case '/second':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child: SecondScreen(
-              title: "Second Screen",
-              color: Colors.redAccent,
-            ),
+          builder: (_) => SecondScreen(
+            title: "Second Screen",
+            color: Colors.redAccent,
           ),
         );
       case '/third':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child: ThirdScreen(
-              title: "Thirst Screen",
-              color: Colors.greenAccent,
-            ),
+          builder: (_) => ThirdScreen(
+            title: "Thirst Screen",
+            color: Colors.greenAccent,
           ),
         );
       default:
         return null;
     }
-  }
-
-  void dispose() {
-    _counterCubit.close();
   }
 }
