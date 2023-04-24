6dd5580207daf943e853d500e829ad1f17dff8af
RayChenCode <miserable55@hotmail.com>
2023-04-24 17:21:03 +0800

    Fix Scaffold to ScaffoldMessenger
    New document shown `Scaffold.of(context).showSnackBar` has been replaced by `ScaffoldMessenger.of(context).showSnackBar`
    
    Reference is https://docs.flutter.dev/release/breaking-changes/scaffold-messenger

--- #3 - Flutter Bloc Concepts/lib/main.dart
+++ #3 - Flutter Bloc Concepts/lib/main.dart
@@ -50,14 +50,14 @@ class _MyHomePageState extends State<MyHomePage> {
             BlocConsumer<CounterCubit, CounterState>(
               listener: (context, state) {
                 if (state.wasIncremented == true) {
-                  Scaffold.of(context).showSnackBar(
+                  ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(
                       content: Text('Incremented!'),
                       duration: Duration(milliseconds: 300),
                     ),
                   );
                 } else if (state.wasIncremented == false) {
-                  Scaffold.of(context).showSnackBar(
+                  ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(
                       content: Text('Decremented!'),
                       duration: Duration(milliseconds: 300),
